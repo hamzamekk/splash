39b0cfa365bf12deed355787bf87fb28cc7ef4b8
Hamza El Mekkoudi <hamza.mekkoudi@gmail.com>
2024-03-02 16:19:16 +0100

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as splash from 'expo-splash-screen';
+import react from 'react';
+
+//this function to prevent splash from being hidden automatically
+splash.preventAutoHideAsync();
+
 export default function App() {
+  react.useEffect(() => {
+    //this function to hide splash based on you needs for example you were waiting to check if there a token or anything
+    const timer = setTimeout(() => {
+      splash.hideAsync();
+      clearTimeout(timer);
+    }, 10000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
